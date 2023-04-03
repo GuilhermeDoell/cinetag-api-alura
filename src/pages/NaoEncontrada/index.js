d0a9ba64c52628e6b3877a884fd03a797599d6fb
Gui <guilhermedoell@outlook.com>
2023-04-03 14:35:50 -0300

    first commit

---
+++ src/pages/NaoEncontrada/index.js
@@ -0,0 +1,12 @@
+import styles from './NaoEncontrada.module.css';
+
+function NaoEncontrada() {
+  return (
+    <section className={styles.container}>
+      <h2>Ops!</h2>
+      <h3>Página não encontrada</h3>
+    </section>
+  )
+}
+
+export default NaoEncontrada;
